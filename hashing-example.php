7b28da91063a02b51e6822fdc9048cf7c19a9f79
Kritika Sharma <35176749+KritikaSharmaKS@users.noreply.github.com>
2022-05-30 20:28:53 -0400

    verify password

--- hashing-example.php
+++ hashing-example.php
@@ -2,7 +2,10 @@
 
 $password = "secret";
 
-$hash = password_hash($password, PASSWORD_DEFAULT);
+// $hash = password_hash($password, PASSWORD_DEFAULT);
 
-echo $hash;
+// echo $hash;
 
+$hash = '$2y$10$l4PaVroYaOgl0WbNV0T5MO8BU7F3eQpagX5glIXXkRpQxv/ji4UKS';
+
+var_dump(password_verify($password, $hash));
